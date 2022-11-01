6c94f23b6bcab0572763e12042d5fdc4ddaffd94
Tatiana Zotova <109228623+Zotova-TA@users.noreply.github.com>
2022-11-02 00:48:47 +0300

    VKR BAUMANA

---
+++ prilozhenie/VoiceRecordFlask-master/FlaskApp/__init__.py
@@ -0,0 +1,17 @@
+﻿"""
+The flask application package.
+"""
+
+#pragma execution_character_set("utf-8")
+
+
+from flask import Flask
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+# Здесь загружаем модели
+
+import FlaskApp.views
---
+++ prilozhenie/VoiceRecordFlask-master/FlaskApp/views.py
@@ -0,0 +1,36 @@
+﻿python3 runserver.py"""
+Routes and views for the flask application.
+"""
+
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime
+from flask import render_template, request, redirect, url_for
+from FlaskApp import app
+from werkzeug.utils import secure_filename
+
+@app.route('/')
+def input():
+    """Отображает форму ввода аудио"""
+    return render_template(
+        'index.html',
+        title='Запись аудио'
+    )
+
+@app.route('/upload_voice', methods=['POST'])
+def upload():
+    """Загрузка файла"""
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'voice' not in request.files:
+            return "Upload failed"
+
+        file = request.files['voice']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join("d:\\Dev\\VisualStudioCode\\upload\\", filename))
+        return f"Upload OK filename={filename}, type={type(file)}"
+
+
+   
+
